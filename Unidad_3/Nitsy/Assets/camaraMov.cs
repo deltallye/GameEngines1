49f988a90a58029d25a31260e7e8691908a81ef2
Evelyn <aneira_bengalski@hotmail.com>
2020-02-24 13:52:20 -0700

    nuevo juego Nisty

---
+++ Unidad_3/Nitsy/Assets/camaraMov.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camaraMov : MonoBehaviour
+{
+    public GameObject nisty;
+    private Vector3 distancia;
+    void Start()
+    {
+        distancia = transform.position - nisty.transform.position;
+    }
+
+    void Update()
+    {
+        transform.position = nisty.transform.position + distancia;
+    }
+}
---
+++ Unidad_3/Nitsy/Assets/nistyMov.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class nistyMov : MonoBehaviour
+{
+    CharacterController controlador;
+
+    //movimiento/gravedad
+    public float speed;
+    public float gravedad;
+    public float fuerzaSalto;
+    public bool grnd;
+    Vector2 velocidad;
+    
+    void Start()
+    {
+        controlador = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //mover con las teclas
+        Vector2 move = new Vector2(Input.GetAxis("Horizontal"), 0);
+        controlador.Move(move * Time.deltaTime * speed);
+        if(move != Vector2.zero)
+        {
+            transform.forward = move;
+        }
+        //gravedad
+        velocidad.y += gravedad * Time.deltaTime;
+        controlador.Move(velocidad * Time.deltaTime);
+        //salto
+        if(controlador.isGrounded && velocidad.y < 0)
+        {
+            velocidad.y = 0;
+        }
+        grnd = controlador.isGrounded;
+        if(Input.GetButton("Jump") && controlador.isGrounded)
+        {
+            velocidad.y += fuerzaSalto;
+
+            if (Input.GetButton("Jump") && controlador.isGrounded == false)
+            {
+                Debug.Log("Si entra");
+                velocidad.y += 20;
+            }
+
+        }
+
+    }
+}
