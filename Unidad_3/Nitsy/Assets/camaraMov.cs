a7a35aac59375896064dc0338ffad0ab1a1d40a1
Evelyn <aneira_bengalski@hotmail.com>
2020-03-04 13:55:19 -0700

    Sigue fallando el trampolin

--- Unidad_3/Nitsy/Assets/camaraMov.cs
+++ Unidad_3/Nitsy/Assets/camaraMov.cs
@@ -4,8 +4,10 @@ using UnityEngine;
 
 public class camaraMov : MonoBehaviour
 {
+    float x, y;
     public GameObject nisty;
     private Vector3 distancia;
+    public GameObject limiteCamara;
     void Start()
     {
         distancia = transform.position - nisty.transform.position;
@@ -13,6 +15,8 @@ public class camaraMov : MonoBehaviour
 
     void Update()
     {
+        
         transform.position = nisty.transform.position + distancia;
+        //transform.position = new Vector2
     }
 }
